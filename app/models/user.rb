af7a4e6d04d14b62ea110a18048260e5b5751e20
Halima N. Muhammad <muhammadhalimah@gmail.com>
2017-09-25 11:20:27 -0400

    working app

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,9 +4,14 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
+
+
          has_many :tweets
 
          mount_uploader :avatar, AvatarUploader
 
          validates :username, presence: true, uniqueness: true
+         serialize :following, Array
+
+
 end
